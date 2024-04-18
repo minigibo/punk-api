57521dd202ef1dd519482609546eedd4d6cf7cfa
mattgibson18@outlook.com <mattgibson18@outlook.com>
2024-04-18 12:31:10 +0100

    styling the cards adding variables

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import beers from "./data/beers";
 const App = () => {
   return (
     <div className="app">
-      <h1>Beer Catalog</h1>
-      <div className="beerList">
+      <h1 className="app__title">Beer Catalog</h1>
+      <div className="app__beerList">
         {beers.map((beer: Beer) => (
           <Card key={beer.id} beer={beer} />
         ))}
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -8,12 +8,20 @@ type CardProps = {
 const Card = ({ beer }: CardProps) => {
   const { name, description, abv, image_url } = beer;
 
+  const cleanedDesc = (value: string) => {
+    if (value.length < 200) {
+      return value;
+    } else {
+      return value.substring(0, value.lastIndexOf(".", 200)) + "...";
+    }
+  };
+
   return (
     <div className="beerCard">
       <img className="beerCard__image" src={image_url} alt={name} />
       <h2 className="beerCard__name">{name}</h2>
       <span className="beerCard__percent">ABV {abv}%</span>
-      <p className="beerCard__desc">{description}</p>
+      <p className="beerCard__desc">{cleanedDesc(description)}</p>
     </div>
   );
 };
